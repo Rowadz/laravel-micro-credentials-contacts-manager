276423800160e7894ee0ffd591ca402a39479f3f
MohammedAl-rowad <rowadwo@gmail.com>
2022-05-23 19:54:05 +0200

    generte breeze files

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\ContactsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', [ContactsController::class, 'create_contacts_view'])->name('home');
 
@@ -29,4 +15,8 @@ Route::post('/save-contacts', [ContactsController::class, 'save_contacts'])->nam
 Route::get('/list-contacts', [ContactsController::class, 'list_contacts'])->name('list-contacts');
 
 
-// Route::get('/create-contacts', [ContactsController::class, 'create_contacts_view']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
